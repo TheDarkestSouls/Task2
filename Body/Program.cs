1d54095d67146e66113a58a1ffac3bf3f40ef1ec
NikitaP <unntiss@bk.ru>
2022-08-06 19:13:42 +0300

    1st try

--- Body/Program.cs
+++ Body/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Enter first number: "); 
+string first = Console.ReadLine();
+Console.Write("Enter second number: "); 
+string second = Console.ReadLine();
+
+int a = first, b = second;
+
+if(a > b)
+{
+    Console.Write(a); Console.Write(" is bigger than "); Console.Write(b); 
+}
+
+if(a < b)
+{
+    Console.Write(b); Console.Write(" is bigger than "); Console.Write(a); 
+}
+
+if(a == b)
+{
+    Console.Write("numbers are equal");
+}
